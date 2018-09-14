41f82183f8f95089160074c8bcb64497960990c7
Neal Nguyen <nealhnguyen@gmail.com>
2018-09-13 23:22:59 -0700

    starting xcode project

---
+++ iOSTouchVisualizer/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  iOSTouchVisualizer
+//
+//  Created by Neal Nguyen on 9/13/18.
+//  Copyright © 2018 Neal Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
